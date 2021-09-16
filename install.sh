a2a2f31a49cfc6d0959c46e11ef256c4ba96ca80
Ajish Antony <48723128+ajish-antony@users.noreply.github.com>
2021-09-16 19:12:55 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-amazon-linux-extras install epel -y
-yum install java-1.8.0-openjdk-devel -y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum -y install jenkins
-systemctl start jenkins
-systemctl enable jenkins
